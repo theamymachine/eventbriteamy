3a246c00091d8a5cb2d68ad35e874ccbd385d316
theamymachine <amymcarthur10@gmail.com>
2020-02-17 18:44:09 +0100

    ok

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,22 @@
+class AttendancesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
