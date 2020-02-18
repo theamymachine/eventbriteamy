14f38a8a10f0a5225f412a3d9271e8e8d1c0de74
theamymachine <amymcarthur10@gmail.com>
2020-02-18 13:31:30 +0100

    sign out ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ require 'faker'
 
 yopmail_array = ["mabite@yopmail.com", "thptoulouse@yopmail.com", "monsieurX@yopmail.com", "Salutmabite@yopmail.com", "jaimemabite@yopmail.com"]
 
-5.times{
-  User.create!(first_name: Faker::FunnyName.name, last_name: Faker::FunnyName.name, email: yopmail_array.sample, description: Faker::Movies::HitchhikersGuideToTheGalaxy.quote)
-  }
+#5.times{
+  #User.create!(first_name: Faker::FunnyName.name, #last_name: Faker::FunnyName.name, email: yopmail_array.#sample, description: #Faker::Movies::HitchhikersGuideToTheGalaxy.quote)
+  #}
 
-5.times{
-  Event.create!(start_date: Faker::Date.forward(days: 30), user: User.all.sample, description: Faker::ChuckNorris.fact, location: Faker::Address.city, title: Faker::Movies::HarryPotter.character, price: rand(50..200), duration: 60)
-  }
+#5.times{
+ # Event.create!(start_date: Faker::Date.forward(days: 30), user: User.all.sample, description: Faker::ChuckNorris.fact, location: Faker::Address.city, title: Faker::Movies::HarryPotter.character, price: rand(50..200), duration: 60)
+  #}
 
-5.times{
-  Attendance.create!(user: User.all.sample, event: Event.all.sample)
-}
+#5.times{
+ # Attendance.create!(user: User.all.sample, event: Event.all.sample)
+#}
